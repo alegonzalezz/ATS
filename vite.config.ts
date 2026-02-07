297ce8c1b48be1e9ececace7fbaaca0aa2ba3808
agonzalezz <alegonzalez@dc.uba.ar>
2026-02-07 12:00:47 -0300

    add deploy and redeploy action

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { inspectAttr } from 'kimi-plugin-inspect-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/ATS',
+  base: '/ATS/',
   plugins: [inspectAttr(), react()],
   resolve: {
     alias: {
